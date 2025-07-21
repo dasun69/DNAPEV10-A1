ecf8ee1f243ab4fedca38a4cb5ee85da5c1b0daa
dasun69 <dasunvithanage4@gmail.com>
2025-07-21 19:55:08 +0530

    create health professional class default constructor and parameterized counstructor

--- src/HealthProfessional.java
+++ src/HealthProfessional.java
@@ -3,6 +3,25 @@ public class HealthProfessional {
     protected String name;
     protected String clinicName;
 
+    // Default constructor
+    public HealthProfessional() {
+        this.id = 0;
+        this.name = "Unknown";
+        this.clinicName = "Unknown Clinic";
+    }
 
+    // Constructor with parameters
+    public HealthProfessional(int id, String name, String clinicName) {
+        this.id = id;
+        this.name = name;
+        this.clinicName = clinicName;
+    }
+
+    // Method create print all instance variables
+    public void printDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Clinic Name: " + clinicName);
+    }
 }
 
